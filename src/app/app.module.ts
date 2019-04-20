587592e442723cf5becc34b7516dff58f5d5c656
Patrick Legare <p_legare@hotmail.com>
2019-04-20 13:10:20 -0400

    Refactoring.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {StoreModule} from '@ngrx/store';
-import {reducers, metaReducers} from './reducers';
+import {reducers, metaReducers} from './store/reducers';
 import {RevenuComponent} from './revenu/revenu.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import { TotalComponent } from './total/total.component';
