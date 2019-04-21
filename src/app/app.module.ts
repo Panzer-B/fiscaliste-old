505341f5d848a13470f79a3cd5d3f0c35a512179
Patrick Legare <p_legare@hotmail.com>
2019-04-21 14:29:52 -0400

    refactoring.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {StoreModule} from '@ngrx/store';
-import {reducers, metaReducers} from './store/reducers';
-import {RevenuComponent} from './income/income.component';
-import {ReactiveFormsModule} from "@angular/forms";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { metaReducers, reducers } from './store/reducers';
+import { IncomeComponent } from './income/income.component';
+import { ReactiveFormsModule } from "@angular/forms";
 import { TotalComponent } from './total/total.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        RevenuComponent,
+        IncomeComponent,
         TotalComponent
     ],
     imports: [
