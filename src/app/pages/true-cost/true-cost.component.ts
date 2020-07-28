47268d0525973e51b82107e12adc140ce0e05b13
Patrick Legare <p_legare@hotmail.com>
2020-07-27 22:01:49 -0400

    [true-cost] - RRSP calculations.

--- src/app/pages/true-cost/true-cost.component.ts
+++ src/app/pages/true-cost/true-cost.component.ts
@@ -12,6 +12,7 @@ import {
     selectPersonWeeklyHours
 } from "../../store/person.selector";
 import { DebounceTime } from "../../app.config";
+import { calculateTaxes } from "../../core/taxes/income.calculator";
 
 @Component({
     selector: 'app-true-cost',
@@ -20,13 +21,22 @@ import { DebounceTime } from "../../app.config";
 })
 export class TrueCostComponent implements OnInit {
 
+    // forms
     formGroup: FormGroup;
     costControl: FormControl;
     hourlyRateControl: FormControl;
     numberOfHoursControl: FormControl;
+
+    // params
+    yearsOfInvestments = 25;
+
+    // results
     fakeResult = 0;
     result = 0;
     compoundValue = 0;
+    compoundValueRRSP = 0;
+    compoundAddedValue = 0;
+    compoundAddedValueRRSP = 0;
 
     constructor(private _store: Store<AppState>) {
     }
@@ -70,13 +80,68 @@ export class TrueCostComponent implements OnInit {
                 debounceTime(1000),
             )
             .subscribe((_value: number) => {
-            const months = 25 * 12;
-            const monthlyRate = Math.round(7/12*1000)/1000;
-            for ( let i = 1; i <= months; i++ ) {
-                _value = _value * (1 + monthlyRate / 100);
-            }
-            this.compoundValue = Math.round(_value * 100) / 100;
-        });
+
+                let grossIncome = 0;
+                this._store.pipe(
+                    select(selectPersonGrossIncome)
+                ).subscribe((_grossIncome) => {
+                    grossIncome = _grossIncome;
+                });
+
+                const netIncome = calculateTaxes(grossIncome);
+
+                const months = this.yearsOfInvestments * 12;
+                const monthlyRate = Math.round(7 / 12 * 1000) / 1000;
+                let compoundValue = _value;
+                let compoundValueRRSP = _value;
+                let compoundAddedValue = _value;
+                let compoundAddedValueRRSP = _value;
+                let rrspTaxReturn = 0;
+                let rrspInvestment = _value;
+
+                let rrspYearlyTaxReturn = 0;
+                let rrspYearlyInvestment = _value;
+
+                for (let i = 1; i <= months; i++) {
+                    // compound
+                    compoundValue = compoundValue * (1 + monthlyRate / 100);
+
+                    // RRSP
+                    if (i % 12 === 0 && i !== 0) {
+                        const trueIncome = calculateTaxes(grossIncome - rrspInvestment);
+                        rrspTaxReturn = Math.round((netIncome - trueIncome) * 100) / 100;
+                        rrspInvestment = rrspTaxReturn;
+
+                        compoundValueRRSP = compoundValueRRSP + rrspTaxReturn * (1 + monthlyRate / 100);
+                    } else {
+                        compoundValueRRSP = compoundValueRRSP * (1 + monthlyRate / 100);
+                    }
+
+                    // Yearly investment
+                    if (i % 12 === 0 && i !== 0) {
+                        compoundAddedValue = compoundAddedValue + _value * (1 + monthlyRate / 100);
+                    } else {
+                        compoundAddedValue = compoundAddedValue * (1 + monthlyRate / 100);
+                    }
+
+                    // Yearly investment RRSP
+                    if (i % 12 === 0 && i !== 0) {
+                        // RRSP
+                        const trueIncome = calculateTaxes(grossIncome - rrspYearlyInvestment);
+                        rrspYearlyTaxReturn = Math.round((netIncome - trueIncome) * 100) / 100;
+                        rrspYearlyInvestment = rrspYearlyTaxReturn + _value;
+                        compoundAddedValueRRSP = compoundAddedValueRRSP + _value + rrspYearlyTaxReturn * (1 + monthlyRate / 100);
+
+                    } else {
+                        compoundAddedValueRRSP = compoundAddedValueRRSP * (1 + monthlyRate / 100);
+                    }
+                }
+                this.compoundValue = Math.round(compoundValue * 100) / 100;
+                this.compoundAddedValue = Math.round(compoundAddedValue * 100) / 100;
+                this.compoundValueRRSP = Math.round(compoundValueRRSP * 100) / 100;
+                this.compoundAddedValueRRSP = Math.round(compoundAddedValueRRSP * 100) / 100;
+
+            });
 
         combineLatest(
             this._store.pipe(
@@ -91,6 +156,4 @@ export class TrueCostComponent implements OnInit {
 
         this.costControl.setValue(10000, {emitEvent: true});
     }
-
-
 }
