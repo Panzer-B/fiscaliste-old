14455a39144f05a0a70036f4a39742df6fab9b3e
Patrick Legare <p_legare@hotmail.com>
2021-08-06 17:30:53 -0400

    select time frequency.

--- apps/fiscaliste/src/app/pages/tax-calculator/tax-calculator.component.ts
+++ apps/fiscaliste/src/app/pages/tax-calculator/tax-calculator.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
-  selector: 'app-tax-calculator',
-  templateUrl: './tax-calculator.component.html',
-  styleUrls: ['./tax-calculator.component.scss']
+    selector: 'app-tax-calculator',
+    templateUrl: './tax-calculator.component.html',
+    styleUrls: ['./tax-calculator.component.scss']
 })
 export class TaxCalculatorComponent implements OnInit {
 
-  constructor() { }
+    formGroup:FormGroup;
+    taxControl: FormControl;
+    isResultVisible:boolean;
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    get taxWidth(){
+        return this.taxControl.value * 800 / 114000;
+    }
 
+    ngOnInit() {
+        this.taxControl = new FormControl(null);
+        this.formGroup = new FormGroup({
+            'taxControl': this.taxControl
+        });
+    }
+    calculate(){
+        this.isResultVisible = true;
+    }
 }
